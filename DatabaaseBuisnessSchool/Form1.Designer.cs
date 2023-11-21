fb97419e4b380bb1a532888f57bdbfd8361f5003
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2023-11-21 12:20:40 +0200

    Add files via upload

---
+++ DatabaaseBuisnessSchool/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace DatabaaseBuisnessSchool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Kat_Box = new System.Windows.Forms.ComboBox();
+            this.Kat_lbl = new System.Windows.Forms.Label();
+            this.Kogus_lbl = new System.Windows.Forms.Label();
+            this.Hind_lbl = new System.Windows.Forms.Label();
+            this.Hind_Box = new System.Windows.Forms.ComboBox();
+            this.Lisa_Katbutt = new System.Windows.Forms.Button();
+            this.Kogus_Box = new System.Windows.Forms.ComboBox();
+            this.Toode_lbl = new System.Windows.Forms.Label();
+            this.Toode_Box = new System.Windows.Forms.ComboBox();
+            this.Kustuta_Katbutt = new System.Windows.Forms.Button();
+            this.Otsi_Katbutt = new System.Windows.Forms.Button();
+            this.LisaLisa_Katbutt = new System.Windows.Forms.Button();
+            this.Uuenda_Katbutt = new System.Windows.Forms.Button();
+            this.KustutaKustuta_Katbutt = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 215);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(602, 186);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Kat_Box
+            // 
+            this.Kat_Box.FormattingEnabled = true;
+            this.Kat_Box.Location = new System.Drawing.Point(174, 127);
+            this.Kat_Box.Name = "Kat_Box";
+            this.Kat_Box.Size = new System.Drawing.Size(121, 21);
+            this.Kat_Box.TabIndex = 1;
+            // 
+            // Kat_lbl
+            // 
+            this.Kat_lbl.AutoSize = true;
+            this.Kat_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Kat_lbl.Location = new System.Drawing.Point(47, 124);
+            this.Kat_lbl.Name = "Kat_lbl";
+            this.Kat_lbl.Size = new System.Drawing.Size(117, 26);
+            this.Kat_lbl.TabIndex = 2;
+            this.Kat_lbl.Text = "Kategooria";
+            // 
+            // Kogus_lbl
+            // 
+            this.Kogus_lbl.AutoSize = true;
+            this.Kogus_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Kogus_lbl.Location = new System.Drawing.Point(83, 61);
+            this.Kogus_lbl.Name = "Kogus_lbl";
+            this.Kogus_lbl.Size = new System.Drawing.Size(74, 26);
+            this.Kogus_lbl.TabIndex = 3;
+            this.Kogus_lbl.Text = "Kogus";
+            // 
+            // Hind_lbl
+            // 
+            this.Hind_lbl.AutoSize = true;
+            this.Hind_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Hind_lbl.Location = new System.Drawing.Point(97, 94);
+            this.Hind_lbl.Name = "Hind_lbl";
+            this.Hind_lbl.Size = new System.Drawing.Size(57, 26);
+            this.Hind_lbl.TabIndex = 4;
+            this.Hind_lbl.Text = "Hind";
+            // 
+            // Hind_Box
+            // 
+            this.Hind_Box.FormattingEnabled = true;
+            this.Hind_Box.Location = new System.Drawing.Point(174, 94);
+            this.Hind_Box.Name = "Hind_Box";
+            this.Hind_Box.Size = new System.Drawing.Size(121, 21);
+            this.Hind_Box.TabIndex = 5;
+            this.Hind_Box.SelectedIndexChanged += new System.EventHandler(this.Hind_Box_SelectedIndexChanged);
+            // 
+            // Lisa_Katbutt
+            // 
+            this.Lisa_Katbutt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Lisa_Katbutt.Location = new System.Drawing.Point(12, 166);
+            this.Lisa_Katbutt.Name = "Lisa_Katbutt";
+            this.Lisa_Katbutt.Size = new System.Drawing.Size(96, 43);
+            this.Lisa_Katbutt.TabIndex = 6;
+            this.Lisa_Katbutt.Text = "Lisa Kategooria";
+            this.Lisa_Katbutt.UseVisualStyleBackColor = true;
+            this.Lisa_Katbutt.Click += new System.EventHandler(this.Lisa_Katbutt_Click);
+            // 
+            // Kogus_Box
+            // 
+            this.Kogus_Box.FormattingEnabled = true;
+            this.Kogus_Box.Location = new System.Drawing.Point(174, 61);
+            this.Kogus_Box.Name = "Kogus_Box";
+            this.Kogus_Box.Size = new System.Drawing.Size(121, 21);
+            this.Kogus_Box.TabIndex = 7;
+            // 
+            // Toode_lbl
+            // 
+            this.Toode_lbl.AutoSize = true;
+            this.Toode_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Toode_lbl.Location = new System.Drawing.Point(13, 24);
+            this.Toode_lbl.Name = "Toode_lbl";
+            this.Toode_lbl.Size = new System.Drawing.Size(155, 26);
+            this.Toode_lbl.TabIndex = 8;
+            this.Toode_lbl.Text = "Toode nimetus";
+            // 
+            // Toode_Box
+            // 
+            this.Toode_Box.FormattingEnabled = true;
+            this.Toode_Box.Location = new System.Drawing.Point(174, 29);
+            this.Toode_Box.Name = "Toode_Box";
+            this.Toode_Box.Size = new System.Drawing.Size(121, 21);
+            this.Toode_Box.TabIndex = 9;
+            // 
+            // Kustuta_Katbutt
+            // 
+            this.Kustuta_Katbutt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Kustuta_Katbutt.Location = new System.Drawing.Point(113, 166);
+            this.Kustuta_Katbutt.Name = "Kustuta_Katbutt";
+            this.Kustuta_Katbutt.Size = new System.Drawing.Size(96, 43);
+            this.Kustuta_Katbutt.TabIndex = 10;
+            this.Kustuta_Katbutt.Text = "Kustuta Kategooria";
+            this.Kustuta_Katbutt.UseVisualStyleBackColor = true;
+            // 
+            // Otsi_Katbutt
+            // 
+            this.Otsi_Katbutt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Otsi_Katbutt.Location = new System.Drawing.Point(214, 166);
+            this.Otsi_Katbutt.Name = "Otsi_Katbutt";
+            this.Otsi_Katbutt.Size = new System.Drawing.Size(96, 43);
+            this.Otsi_Katbutt.TabIndex = 11;
+            this.Otsi_Katbutt.Text = "Otsi Fail";
+            this.Otsi_Katbutt.UseVisualStyleBackColor = true;
+            // 
+            // LisaLisa_Katbutt
+            // 
+            this.LisaLisa_Katbutt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.LisaLisa_Katbutt.Location = new System.Drawing.Point(315, 166);
+            this.LisaLisa_Katbutt.Name = "LisaLisa_Katbutt";
+            this.LisaLisa_Katbutt.Size = new System.Drawing.Size(96, 43);
+            this.LisaLisa_Katbutt.TabIndex = 12;
+            this.LisaLisa_Katbutt.Text = "Lisa";
+            this.LisaLisa_Katbutt.UseVisualStyleBackColor = true;
+            // 
+            // Uuenda_Katbutt
+            // 
+            this.Uuenda_Katbutt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Uuenda_Katbutt.Location = new System.Drawing.Point(416, 166);
+            this.Uuenda_Katbutt.Name = "Uuenda_Katbutt";
+            this.Uuenda_Katbutt.Size = new System.Drawing.Size(96, 43);
+            this.Uuenda_Katbutt.TabIndex = 13;
+            this.Uuenda_Katbutt.Text = "Uuenda ";
+            this.Uuenda_Katbutt.UseVisualStyleBackColor = true;
+            // 
+            // KustutaKustuta_Katbutt
+            // 
+            this.KustutaKustuta_Katbutt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.KustutaKustuta_Katbutt.Location = new System.Drawing.Point(518, 166);
+            this.KustutaKustuta_Katbutt.Name = "KustutaKustuta_Katbutt";
+            this.KustutaKustuta_Katbutt.Size = new System.Drawing.Size(96, 43);
+            this.KustutaKustuta_Katbutt.TabIndex = 14;
+            this.KustutaKustuta_Katbutt.Text = "Kustuta";
+            this.KustutaKustuta_Katbutt.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 415);
+            this.Controls.Add(this.KustutaKustuta_Katbutt);
+            this.Controls.Add(this.Uuenda_Katbutt);
+            this.Controls.Add(this.LisaLisa_Katbutt);
+            this.Controls.Add(this.Otsi_Katbutt);
+            this.Controls.Add(this.Kustuta_Katbutt);
+            this.Controls.Add(this.Toode_Box);
+            this.Controls.Add(this.Toode_lbl);
+            this.Controls.Add(this.Kogus_Box);
+            this.Controls.Add(this.Lisa_Katbutt);
+            this.Controls.Add(this.Hind_Box);
+            this.Controls.Add(this.Hind_lbl);
+            this.Controls.Add(this.Kogus_lbl);
+            this.Controls.Add(this.Kat_lbl);
+            this.Controls.Add(this.Kat_Box);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox Kat_Box;
+        private System.Windows.Forms.Label Kat_lbl;
+        private System.Windows.Forms.Label Kogus_lbl;
+        private System.Windows.Forms.Label Hind_lbl;
+        private System.Windows.Forms.ComboBox Hind_Box;
+        private System.Windows.Forms.Button Lisa_Katbutt;
+        private System.Windows.Forms.ComboBox Kogus_Box;
+        private System.Windows.Forms.Label Toode_lbl;
+        private System.Windows.Forms.ComboBox Toode_Box;
+        private System.Windows.Forms.Button Kustuta_Katbutt;
+        private System.Windows.Forms.Button Otsi_Katbutt;
+        private System.Windows.Forms.Button LisaLisa_Katbutt;
+        private System.Windows.Forms.Button Uuenda_Katbutt;
+        private System.Windows.Forms.Button KustutaKustuta_Katbutt;
+    }
+}
+
---
+++ DatabaaseBuisnessSchool/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaaseBuisnessSchool
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\AppData\Tooded_DB.mdf;Integrated Security=True");
+        SqlDataAdapter adapter_toode, adapter_kategooria;
+        SqlCommand command;
+        public Form1()
+        {
+            InitializeComponent();
+            NaitaAndmed();
+            NaitaKategooria();
+        }
+
+
+        public void NaitaAndmed()
+        {
+            connect.Open();
+            DataTable dt_toode = new DataTable();
+            adapter_toode = new SqlDataAdapter("SELECT * FROM Toode", connect);
+            adapter_toode.Fill(dt_toode);
+            dataGridView1.DataSource= dt_toode;
+            connect.Close();
+
+        }
+        public void NaitaKategooria()
+        {
+            connect.Open();
+            adapter_kategooria = new SqlDataAdapter("SELECT Kategooria_nimetus FROM Kategooriad", connect);
+            DataTable dt_kat = new DataTable();
+            adapter_kategooria.Fill(dt_kat);
+            foreach (DataRow item in dt_kat.Rows)
+            {
+                Kat_Box.Items.Add(item["Kategooria_nimetus"]);
+            }
+            connect.Close();
+        }
+
+        private void Hind_Box_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            connect.Open();
+            adapter_toode = new SqlDataAdapter("SELECT Hind FROM Toode", connect);
+            DataTable dt_kat = new DataTable();
+            adapter_toode.Fill(dt_kat);
+            foreach (DataRow item in dt_kat.Rows)
+            {
+                Hind_Box.Items.Add(item["Hind"]);
+            }
+            connect.Close();
+        }
+
+        private void Lisa_Katbutt_Click(object sender, EventArgs e)
+        {
+            command = new SqlCommand("INSERT INTO Kategooriad (Kategooria_nimetus) VALUES (@kat)", connect);
+            connect.Open();
+            command.Parameters.AddWithValue("@kat", Kat_Box.Text);
+            command.ExecuteNonQuery();
+            connect.Close();
+            Kat_Box.Items.Clear();
+            NaitaKategooria();
+        }
+    }
+}
